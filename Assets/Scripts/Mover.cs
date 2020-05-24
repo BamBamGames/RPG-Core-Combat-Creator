a50a9fe6ba66efb42d7ff922e216960293132c66
lightjiao <lightjiao@gmail.com>
2020-05-24 22:52:47 +0800

    :sparkles: (Player): Make player move to the mouse click

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,6 +10,21 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        bool HasHit = Physics.Raycast(ray, out hit);
+        if (HasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
