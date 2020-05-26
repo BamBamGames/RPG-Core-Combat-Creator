4164bb871aa21ba6266dad31aa67f07defa83336
lightjiao <lightjiao@gmail.com>
2020-05-26 16:39:53 +0800

    :sparkles: (25): Separating The Control Layer

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+    }
+    
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        bool HasHit = Physics.Raycast(ray, out hit);
+        if (HasHit)
+        {
+            GetComponent<Mover>().MoveTo(hit.point);
+        }
+    }
+}
