705ae53a416a5702322d9b5ec99c4d0b1762db66
lightjiao <lightjiao@gmail.com>
2020-05-30 16:40:52 +0800

    :sparkles: (54): Patrolling Behaviour

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -18,7 +18,7 @@ namespace RPG.Control
             }
         }
 
-        private int GetNextIndex(int i)
+        public int GetNextIndex(int i)
         {
             int j = i + 1;
             if (j >= transform.childCount) j = 0;
@@ -26,7 +26,7 @@ namespace RPG.Control
             return j;
         }
 
-        private Vector3 GetWaypoint(int i)
+        public Vector3 GetWaypoint(int i)
         {
             return transform.GetChild(i).position;
         }
