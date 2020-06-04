5c4f58792bebc7474847760a9ec11cda2e8de0ae
lightjiao <lightjiao@gmail.com>
2020-06-04 23:33:45 +0800

    :sparkles: (114): Make A Weapon Projectiles

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private Transform target = null;
+    [SerializeField] private float speed = 7f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (target == null) return;
+
+        transform.LookAt(GetAimLocation());
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private Vector3 GetAimLocation()
+    {
+        Vector3 aimPosition = target.position;
+
+        var targetCapsule = target.GetComponent<CapsuleCollider>();
+        if (targetCapsule != null)
+        {
+            aimPosition += Vector3.up * targetCapsule.height / 2;
+        }
+
+        return aimPosition;
+    }
+}
