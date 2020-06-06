158da9db5dba1ab3fa19cdd6dac0bcefc891e3ad
lightjiao <lightjiao@gmail.com>
2020-06-07 01:21:29 +0800

    :sparkles: (129): Progression Scriptable Object

---
+++ Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace RPG.Stats
+{
+    [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
+    public class Progression : ScriptableObject
+    {
+        [SerializeField] private ProgressionCharacterClass[] characterClasses = null;
+
+        /**
+         * if we don't have [System.Serializable]
+         * Assets\Scripts\Stats\Progression.cs(13,17): warning CS0414: The field 'Progression.ProgressionCharacterClass.value' is assigned but its value is never used
+         * this class here is not marked as something that unity knows how to serialize
+         * use attribute [System.Serializable]
+         */
+        [System.Serializable]
+        class ProgressionCharacterClass
+        {
+            [SerializeField] public CharacterClass characterClass;
+            [SerializeField] public float[] health;
+        }
+    }
+}
