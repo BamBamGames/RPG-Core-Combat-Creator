6bf5e9a3e6fa8ed69b15769be532aa1f0564c772
lightjiao <lightjiao@gmail.com>
2020-06-12 01:04:51 +0800

    :sparkles: (163): Spawning Damage Text

---
+++ Assets/Scripts/UI/DamageText/DamageText.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace RPG.UI.DamageText
+{
+    public class DamageText : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace RPG.UI.DamageText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] private DamageText damageTextPrefab = null;
+
+        private void Start()
+        {
+            Spawn(11);
+        }
+
+        public void Spawn(float damage)
+        {
+            var damageTextInstance = Instantiate<DamageText>(damageTextPrefab, this.transform.position, this.transform.rotation);
+        }
+    }
+}
