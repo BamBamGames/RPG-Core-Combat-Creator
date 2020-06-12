ee25f3d201ccd8bb1dac82edbb6844ed5e695450
lightjiao <lightjiao@gmail.com>
2020-06-13 02:19:51 +0800

    :sparkles: (169): Disabling Empty Health Bars

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace RPG.Attributes
 {
@@ -7,11 +6,19 @@ namespace RPG.Attributes
     {
         [SerializeField] Health healthComponent = null;
         [SerializeField] RectTransform foreground = null;
+        [SerializeField] Canvas rootCanvas = null;
 
         private void Update()
         {
-            var percentage = healthComponent.GetPercentage();
-            foreground.localScale = new Vector3(percentage, 1, 1);
+            var healthPercentage = healthComponent.GetPercentage();
+            if (Mathf.Approximately(healthPercentage, 0) || Mathf.Approximately(healthPercentage, 1))
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+
+            rootCanvas.enabled = true;
+            foreground.localScale = new Vector3(healthPercentage, 1, 1);
         }
     }
 }
