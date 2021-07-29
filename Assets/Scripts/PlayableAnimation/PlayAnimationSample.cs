cd1772b240f89edcb32275b2c7cfec5cac9013fa
lightjiao <lightjiao@gmail.com>
2021-07-29 18:32:03 +0800

    简化playable的代码

--- Assets/Scripts/PlayableAnimation/PlayAnimationSample.cs
+++ Assets/Scripts/PlayableAnimation/PlayAnimationSample.cs
@@ -13,16 +13,19 @@ namespace RPG.PlayableAnimation
 
         private void Start()
         {
-            // 创建一个PlayableGraph并给它命名
-            playableGraph = PlayableGraph.Create("PlayAnimationSample");
-            // 创建一个Output节点，类型是Animation，名字是Animation，目标对象是物体上的Animator组件
-            var playableOutput = AnimationPlayableOutput.Create(playableGraph, "Animation", GetComponent<Animator>());
-            // 创建一个动画剪辑Playable，将clip传入进去
-            var clipPlayable = AnimationClipPlayable.Create(playableGraph, clip);
-            // 将playable连接到output
-            playableOutput.SetSourcePlayable(clipPlayable);
-            // 播放这个graph
-            playableGraph.Play();
+            // // 创建一个PlayableGraph并给它命名
+            // playableGraph = PlayableGraph.Create("PlayAnimationSample");
+            // // 创建一个Output节点，类型是Animation，名字是Animation，目标对象是物体上的Animator组件
+            // var playableOutput = AnimationPlayableOutput.Create(playableGraph, "Animation", GetComponent<Animator>());
+            // // 创建一个动画剪辑Playable，将clip传入进去
+            // var clipPlayable = AnimationClipPlayable.Create(playableGraph, clip);
+            // // 将playable连接到output
+            // playableOutput.SetSourcePlayable(clipPlayable);
+            // // 播放这个graph
+            // playableGraph.Play();
+
+            // 上面一坨可以简化为这一行代码
+            AnimationPlayableUtilities.PlayClip(GetComponent<Animator>(), clip, out playableGraph);
         }
 
         private void OnDisable()
